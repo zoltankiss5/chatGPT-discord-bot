6a94079ddeb1f3375b04d3e6c9e8e602cfff7f4a
Laszlo.Nagy1 <Laszlo.Nagy1@wolterskluwer.com>
2023-05-10 17:12:21 +0200

    change keyword

--- src/aclient.py
+++ src/aclient.py
@@ -85,7 +85,7 @@ class aclient(discord.Client):
                 response = ""
             if self.chat_model == "OFFICIAL":
                 ai_response = await responses.official_handle_response(user_message, self)
-                if "summary" == user_message.strip().lower():
+                if "history" == user_message.strip().lower():
                     persisted_record = {"ai_summary": ai_response, "timestamp": dt.datetime.utcnow()}
                     print(await db["code-games"][str(os.getenv("REPLYING_ALL_DISCORD_CHANNEL_ID"))].insert_one(persisted_record))
                 response = f"{response}{ai_response}"
